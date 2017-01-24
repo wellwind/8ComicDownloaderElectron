8fe9528548947e09e6785d83f0d2ba961b4de0a8
wellwind <wellwind@mail2000.com.tw>
2017-01-25 00:42:59 +0800

    ComicDownloaderService should call fs.readFile when calling readSettings

--- src/app/comic-downloader/comic-downloader.service.spec.ts
+++ src/app/comic-downloader/comic-downloader.service.spec.ts
@@ -1,14 +1,16 @@
+import { ElectronService } from './../shared/services/electron.service';
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ComicDownloaderService } from './comic-downloader.service';
 const os = window.require('os');
+const fs = window.require('fs');
 
 describe('ComicDownloaderService', () => {
 
   beforeEach(() => {
     spyOn(os, 'homedir').and.returnValue('/foo/bar');
-    
+
     TestBed.configureTestingModule({
       providers: [ComicDownloaderService]
     });
@@ -17,4 +19,13 @@ describe('ComicDownloaderService', () => {
   it('should have basic settings file path', inject([ComicDownloaderService], (service: ComicDownloaderService) => {
     expect(service.getConfigFilePath()).toBe('/foo/bar/8ComicDownloader/settings.conf');
   }));
+
+  it('should call fs.readFile when calling readSettings', done => {
+    const service = TestBed.get(ComicDownloaderService) as ComicDownloaderService;
+    spyOn(fs, 'readFile').and.callFake((err, result) => {
+      expect(true).toBeTruthy();
+      done();
+    });
+    service.readSettings();
+  });
 });
--- src/app/comic-downloader/comic-downloader.service.ts
+++ src/app/comic-downloader/comic-downloader.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 const os = window.require('os');
+const fs = window.require('fs');
 
 @Injectable()
 export class ComicDownloaderService {
   constructor() { }
 
   getConfigFilePath() {
+    
     return os.homedir() + '/8ComicDownloader/settings.conf';;
+  }
+
+  readSettings() {
+    fs.readFile('foo', function(err, result){
+
+    });
   }
 }
