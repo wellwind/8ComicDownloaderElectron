5de05fa73beaad2d93f69cd2be0f90f1095853f7
wellwind <wellwind@mail2000.com.tw>
2017-01-25 00:10:13 +0800

    ComicDownloaderService should have basic settings file path

--- src/app/comic-downloader/comic-downloader.service.spec.ts
+++ src/app/comic-downloader/comic-downloader.service.spec.ts
@@ -2,15 +2,19 @@
 
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ComicDownloaderService } from './comic-downloader.service';
+const os = window.require('os');
 
 describe('ComicDownloaderService', () => {
+
   beforeEach(() => {
+    spyOn(os, 'homedir').and.returnValue('/foo/bar');
+    
     TestBed.configureTestingModule({
       providers: [ComicDownloaderService]
     });
   });
 
-  it('should ...', inject([ComicDownloaderService], (service: ComicDownloaderService) => {
-    expect(service).toBeTruthy();
+  it('should have basic settings file path', inject([ComicDownloaderService], (service: ComicDownloaderService) => {
+    expect(service.getConfigFilePath()).toBe('/foo/bar/8ComicDownloader/settings.conf');
   }));
 });
--- src/app/comic-downloader/comic-downloader.service.ts
+++ src/app/comic-downloader/comic-downloader.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
+const os = window.require('os');
 
 @Injectable()
 export class ComicDownloaderService {
-
   constructor() { }
 
+  getConfigFilePath() {
+    return os.homedir() + '/8ComicDownloader/settings.conf';;
+  }
 }
