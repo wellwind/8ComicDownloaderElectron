9d1d95f942f708302e2f25f6c1f962b63fd052f6
wellwind <wellwind@mail2000.com.tw>
2017-01-24 17:22:41 +0800

    add progress-bar-component

---
+++ src/app/shared/components/progress-bar/progress-bar.component.spec.ts
@@ -0,0 +1,38 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { ProgressBarComponent } from './progress-bar.component';
+
+describe('ProgressBarComponent', () => {
+  let component: ProgressBarComponent;
+  let fixture: ComponentFixture<ProgressBarComponent>;
+  let progressBarDebug: DebugElement;
+  let progressBar: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ProgressBarComponent]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProgressBarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    progressBarDebug = fixture.debugElement.query(By.css('.progress-bar'));
+    progressBar = progressBarDebug.nativeElement;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should has default bar style `success`', () => {
+    fixture.detectChanges();
+
+    expect(progressBar.classList).toContain('progress-bar-success');
+  });
+});
---
+++ src/app/shared/components/progress-bar/progress-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
