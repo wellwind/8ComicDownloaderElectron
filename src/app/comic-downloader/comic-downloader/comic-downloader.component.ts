abcd4870e7b9c56b2b9a144844adabfc6c46ff93
wellwind <wellwind@mail2000.com.tw>
2017-03-14 23:36:43 +0800

    [ComicDownloaderComponent] Observe download progress

--- src/app/comic-downloader/comic-downloader/comic-downloader.component.ts
+++ src/app/comic-downloader/comic-downloader/comic-downloader.component.ts
@@ -1,3 +1,4 @@
+import { ComicImageDownloadStatus } from '../../shared/enums/comic-image-download-status.enum';
 import { ElectronService } from './../../shared/services/electron.service';
 import { ComicDownloaderService } from './../comic-downloader.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,11 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComicDownloaderComponent implements OnInit {
   appSettings: any;
+  downloadProgrsss = 0;
   constructor(private service: ComicDownloaderService) { }
 
   ngOnInit() {
     this.service.readSettingsPromise().then(() => {
       this.appSettings = this.service.appSettings;
+    });
+
+    this.service.downloadProgress.subscribe(num => {
+      this.downloadProgrsss = num;
     });
   }
 }
